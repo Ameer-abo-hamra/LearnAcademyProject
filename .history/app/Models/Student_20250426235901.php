2c9d2e507e3bf6c7c56a2113702895a12397ca01
Ameer Abo Hamra <ameerabohamra314017@gmail.com>
2025-04-27 13:17:26 +0300

    4/27/2025

---
+++ .history/app/Models/Student_20250426235901.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Student extends Authenticatable implements JWTSubject
+{
+    protected $fillable = ["full_name", "admin_activation", "username", "password", "email", "id", "activation_code", "is_active", "free_points"];
+    protected $appends = ['points'];
+
+    public function getPointsAttribute()
+{
+    return $this->free_points + $this->paid_points;
+}
+
+    public function addFreePoints($points = 1)
+    {
+        if ($this->free_points >= 10) {
+            return; // لا شيء يحدث
+        }
+
+        $newPoints = $this->free_points + $points;
+        $this->update([
+            'free_points' => min(10, $newPoints)
+        ]);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, "course_student", "student_id", "course_id");
+    }
+}
---
+++ .history/database/migrations/2025_04_10_113307_create_students_table_20250426234226.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("full_name");
+            $table->unsignedTinyInteger('free_points')->default(0);
+            $table->string("email");
+            $table->string("password");
+            $table->string("username");
+            $table->string("activation_code");
+            $table->boolean("is_active")->default(false);
+            $table->boolean("admin_activation")->default(false);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
